002288fd1998b1fcfefbc5de6d2e7b735e3bd19f
vlldnt <vieilledent.adrien@gmail.com>
2026-01-19 10:20:33 +0100

    feat: first use of Webpack

---
+++ Webpack/task_0/src/index.js
@@ -0,0 +1,12 @@
+import $ from "jquery";
+
+if (typeof window !== "undefined") {
+  window.$ = window.jQuery = $;
+}
+
+$(function () {
+  const p1 = $("<p>").text("Holberton Dashboard");
+  const p2 = $("<p>").text("Dashboard data for the students");
+  const p3 = $("<p>").text("Copyright - Holberton School");
+  $("body").append(p1, p2, p3);
+});
