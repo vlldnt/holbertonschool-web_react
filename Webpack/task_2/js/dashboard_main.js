3f38d7a10b0466044542086253ce410eff48a0e6
vlldnt <vieilledent.adrien@gmail.com>
2026-01-19 14:04:02 +0100

    feat: trying to solve requierement fail

--- Webpack/task_2/js/dashboard_main.js
+++ Webpack/task_2/js/dashboard_main.js
@@ -1,18 +1,20 @@
-const $ = require('jquery');
-const _ = require('lodash');
-require('../css/main.css');
+import $ from 'jquery';
+import { debounce } from 'lodash';
+import '../css/main.css';
 
-let count = 0;
+let counter = 0;
+
 function updateCounter() {
-  count += 1;
-  $('#count').text(`${count} clicks on the button`);
+  counter += 1;
+  $('#count').text(`${counter} clicks on the button`);
 }
+
 $(function () {
-  $('body').prepend('<div id="logo"></div>');
+  $('body').append('<div id="logo" ></div');
   $('body').append('<p>Holberton Dashboard</p>');
   $('body').append('<p>Dashboard data for the students</p>');
   $('body').append('<button>Click here to get started</button>');
-  $('body').append("<p id='count'></p>");
+  $('body').append('<p id="count"></p>');
   $('body').append('<p>Copyright - Holberton School</p>');
   $('button').on('click', _.debounce(updateCounter, 500));
 });
--- Webpack/task_2/webpack.config.js
+++ Webpack/task_2/webpack.config.js
@@ -3,8 +3,8 @@ const path = require('path');
 module.exports = {
   entry: './js/dashboard_main.js',
   output: {
-    path: path.resolve(__dirname, 'public'),
     filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'),
   },
   mode: 'production',
   module: {
@@ -14,11 +14,17 @@ module.exports = {
         use: ['style-loader', 'css-loader'],
       },
       {
-        test: /\.(gif|png|jpe?g|svg)$/i,
-        type: 'asset/resource',
-        generator: {
-          filename: 'images/[name][ext]',
-        },
+        test: /\.(gif|jpe?g|png|svg)$/i,
+        use: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              bypassOnDebug: true,
+              disable: true,
+            },
+          },
+        ],
       },
     ],
   },
