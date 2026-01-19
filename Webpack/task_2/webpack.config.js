e823f79983c6cfce3ff0bfe5ef4c27944eaebc05
vlldnt <vieilledent.adrien@gmail.com>
2026-01-19 13:39:50 +0100

    fix: add CSS loader configuration to Webpack

--- Webpack/task_2/webpack.config.js
+++ Webpack/task_2/webpack.config.js
@@ -10,6 +10,10 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
         test: /\.(gif|png|jpe?g|svg)$/i,
         use: [
           'file-loader',
