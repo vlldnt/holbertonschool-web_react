8693e70c6f283bd547f45374100856016e695906
vlldnt <vieilledent.adrien@gmail.com>
2026-01-19 10:48:32 +0100

    fix: change jQuery import to CommonJS syntax

--- Webpack/task_0/src/index.js
+++ Webpack/task_0/src/index.js
@@ -1,4 +1,8 @@
-import $ from 'jquery';
+const $ = require( "jquery" );
+
+if (typeof window !== "undefined") {
+  window.$ = window.jQuery = $;
+}
 
 $('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard <strong>data for</strong> the students</p>');
