169f38c91d6ee908b9da871b424cb2096e27a49e
vlldnt <vieilledent.adrien@gmail.com>
2026-01-19 10:46:56 +0100

    feat: refactor simpliest way to write it

--- Webpack/task_0/src/index.js
+++ Webpack/task_0/src/index.js
@@ -1,12 +1,5 @@
-const $ = require( "jquery" );
+import $ from 'jquery';
 
-if (typeof window !== "undefined") {
-  window.$ = window.jQuery = $;
-}
-
-$(function () {
-  const p1 = $("<p>").text("Holberton Dashboard");
-  const p2 = $("<p>").html("Dashboard <strong>data for</strong> the students");
-  const p3 = $("<p>").text("Copyright - Holberton School");
-  $("body").append(p1, p2, p3);
-});
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard <strong>data for</strong> the students</p>');
+$('body').append('<p>Copyright - Holberton School</p>');
