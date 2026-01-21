a7b5fb9cbf555b962491e200e2f839ebde5a284f
vlldnt <vieilledent.adrien@gmail.com>
2026-01-21 10:34:43 +0100

    refactor: config webpack

--- Webpack/task_3/webpack.config.js
+++ Webpack/task_3/webpack.config.js
@@ -1,6 +1,6 @@
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: {
@@ -11,22 +11,15 @@ module.exports = {
     ],
   },
   output: {
-    path: path.resolve(__dirname, 'public'),
+    path: path.resolve(__dirname, './public'),
     filename: '[name].bundle.js',
   },
-  mode: 'development',
-  devtool: 'inline-source-map',
-  optimization: {
-    splitChunks: {
-      chunks: 'all',
-    },
-  },
-  devServer: {
-    static: {
-      directory: path.resolve(__dirname, 'public'),
-    },
-    port: 8564,
-  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Holberton Dashboard',
+    }),
+  ],
   module: {
     rules: [
       {
@@ -34,18 +27,16 @@ module.exports = {
         use: ['style-loader', 'css-loader'],
       },
       {
-        test: /\.(gif|jpe?g|png|svg)$/i,
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
-        generator: {
-          filename: 'images/[name][ext]',
-        },
+        use: ['file-loader'],
       },
     ],
   },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      title: 'Holberton Webpack',
-    }),
-  ],
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './public',
+    port: 8564,
+  },
+  mode: 'development',
 };
