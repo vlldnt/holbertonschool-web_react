16690fd11705382627eb2925a6a4e5d5c91960bb
vlldnt <vieilledent.adrien@gmail.com>
2026-01-26 16:22:24 +0100

    feat: task 3 new utils tests

---
+++ react_intro/task_3/dashboard/src/Notifications.jsx
@@ -0,0 +1,27 @@
+import closeIcon from './assets/close-button.png';
+import { getLatestNotifications } from './utils';
+
+function Notifications() {
+  function handleClick() {
+    console.log('Close button has been clicked');
+  }
+
+  return (
+    <div className="notifications-items">
+      <p>Here is the list of notifications</p>
+      <ul>
+        <li data-priority="1">New course available</li>
+        <li data-priority="2">New resume available</li>
+        <li
+          data-priority="3"
+          dangerouslySetInnerHTML={{ __html: getLatestNotifications() }}
+        ></li>
+      </ul>
+      <button aria-label="Close" onClick={handleClick}>
+        <img src={closeIcon} alt="close button" width="12px" height="12px" />
+      </button>
+    </div>
+  );
+}
+
+export default Notifications;
