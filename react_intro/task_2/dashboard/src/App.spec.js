17b873c6ccd0b94a82e754ceeb91c8b1ce275bc1
vlldnt <vieilledent.adrien@gmail.com>
2026-01-26 14:45:50 +0100

    feat: test label and input email

--- react_intro/task_2/dashboard/src/App.spec.js
+++ react_intro/task_2/dashboard/src/App.spec.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('"School Dashboard" written in a h1', async () => {
+test('"School Dashboard" written in a h1', () => {
   render(<App />);
   const text = screen.getByRole('heading', {
     level: 1,
@@ -10,10 +10,16 @@ test('"School Dashboard" written in a h1', async () => {
   expect(text).toBeInTheDocument();
 });
 
-test('text content within the 2 p elements', async () => {
-  render(<App />);
-  const paragraph = screen.getAllByText(/./, { selector: 'p' });
-  expect(paragraph.length).toBe(2);
+test('text content within the 2 p elements', () => {
+  const { container } = render(<App />);
+  const appBodyP = container.querySelector('.App-body p');
+  const appFooterP = container.querySelector('.App-footer p');
+
+  expect(appBodyP).toBeInTheDocument();
+  expect(appBodyP.textContent).toBe('Login to access the full dashboard');
+
+  expect(appFooterP).toBeInTheDocument();
+  expect(appFooterP.textContent).toMatch(/Copyright \d{4}/);
 });
 
 test('Is an image rendered', () => {
@@ -22,4 +28,19 @@ test('Is an image rendered', () => {
   expect(image.length).toBe(1);
   const alt = image[0].getAttribute('alt') || '';
   expect(alt.toLowerCase()).toBe('holberton logo');
+});
+
+test('renders 2 input elements', () => {
+  render(<App />);
+  const emailRegex = /e[-\s]*mail[\s:-]*/i;
+  const emailInput = screen.getByLabelText(emailRegex);
+
+  const passwordRegex = /password[\s:-]*/i;
+  const passwordInput = screen.getByLabelText(passwordRegex);
+
+  expect(emailInput).toBeInTheDocument();
+  expect(emailInput).toHaveAttribute('type', 'email');
+
+  expect(passwordInput).toBeInTheDocument();
+  expect(passwordInput).toHaveAttribute('type', 'password');
 });
