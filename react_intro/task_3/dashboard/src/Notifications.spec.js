c69f85698a0ed5319ed9332d52b76e814132b286
vlldnt <vieilledent.adrien@gmail.com>
2026-01-28 10:47:29 +0100

    fix :fix * fix / fix ** fix

--- react_intro/task_3/dashboard/src/Notifications.spec.js
+++ react_intro/task_3/dashboard/src/Notifications.spec.js
@@ -27,6 +27,6 @@ describe('Notifications tests', () => {
     render(<Notifications />);
     const button = screen.getByRole('button', { name: /Close/i });
     fireEvent.click(button);
-    expect(consoleSpy).toHaveBeenCalledWith('Close button has been clicked');
+    expect(consoleSpy).toHaveBeenCalledWith(expect.stringMatching(/Close button has been clicked/i));
   });
 });
