b1dcd5240b58674dd9a33590593fbcd89e4bd01d
vlldnt <vieilledent.adrien@gmail.com>
2026-01-29 09:49:31 +0100

    fix: update ESLint configuration and linting command
    - Refactored ESLint configuration for improved readability and consistency.
    - Changed global variables definition to use spread operator for browser globals only.
    - Updated linting command in package.json to lint the entire project directory instead of just the src folder.

--- react_intro/task_5/dashboard/eslint.config.js
+++ react_intro/task_5/dashboard/eslint.config.js
@@ -1,11 +1,11 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import { defineConfig, globalIgnores } from 'eslint/config';
 
 export default defineConfig([
-  globalIgnores(['dist', '.vite']),
+  globalIgnores(['dist']),
   {
     files: ['**/*.{js,jsx}'],
     extends: [
@@ -15,10 +15,7 @@ export default defineConfig([
     ],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: {
-        ...globals.browser,
-        ...globals.jest,
-      },
+      globals: globals.browser,
       parserOptions: {
         ecmaVersion: 'latest',
         ecmaFeatures: { jsx: true },
@@ -26,7 +23,10 @@ export default defineConfig([
       },
     },
     rules: {
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]', args: 'none' }],
+      'no-unused-vars': [
+        'error',
+        { varsIgnorePattern: '^[A-Z_]', args: 'none' },
+      ],
     },
   },
-])
+]);
