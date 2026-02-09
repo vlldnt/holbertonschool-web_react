91a0f64231d0197fd786e8e2c00a4a58ad3f6d24
vlldnt <vieilledent.adrien@gmail.com>
2026-02-09 10:08:22 +0100

    feat: change function to class react

--- react_component/task_0/dashboard/src/App/App.jsx
+++ react_component/task_0/dashboard/src/App/App.jsx
@@ -1,4 +1,5 @@
 import './App.css';
+import React from 'react';
 import { getLatestNotification } from '../utils/utils.js';
 import Notifications from '../Notifications/Notifications.jsx';
 import Header from '../Header/Header.jsx';
@@ -6,27 +7,30 @@ import Login from '../Login/Login.jsx';
 import Footer from '../Footer/Footer.jsx';
 import CourseList from '../CourseList/CourseList.jsx';
 
-function App({ isLoggedIn = false }) {
-  const notificationsList = [
-    { id: 1, type: 'default', value: 'New course available' },
-    { id: 2, type: 'urgent', value: 'New resume available' },
-    { id: 3, type: 'urgent', html: getLatestNotification() },
-  ];
+class App extends React.Component {
+  render() {
+    const { isLoggedIn = false } = this.props;
+    const notificationsList = [
+      { id: 1, type: 'default', value: 'New course available' },
+      { id: 2, type: 'urgent', value: 'New resume available' },
+      { id: 3, type: 'urgent', html: getLatestNotification() },
+    ];
 
-  const coursesList = [
-    { id: 1, name: 'ES6', credit: 60 },
-    { id: 2, name: 'Webpack', credit: 20 },
-    { id: 3, name: 'React', credit: 40 },
-  ];
+    const coursesList = [
+      { id: 1, name: 'ES6', credit: 60 },
+      { id: 2, name: 'Webpack', credit: 20 },
+      { id: 3, name: 'React', credit: 40 },
+    ];
 
-  return (
-    <>
-      <Notifications notifications={notificationsList} />
-      <Header />
-      {isLoggedIn ? <CourseList courses={coursesList} /> : <Login />}
-      <Footer />
-    </>
-  );
+    return (
+      <>
+        <Notifications notifications={notificationsList} />
+        <Header />
+        {isLoggedIn ? <CourseList courses={coursesList} /> : <Login />}
+        <Footer />
+      </>
+    );
+  }
 }
 
 export default App;
