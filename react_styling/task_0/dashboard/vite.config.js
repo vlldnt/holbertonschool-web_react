d53c4e92c4586c937ca0568b7ba6366e10478916
vlldnt <vieilledent.adrien@gmail.com>
2026-02-10 13:48:33 +0100

    feat: taiwincss / roboto fotn family to the project

--- react_styling/task_0/dashboard/vite.config.js
+++ react_styling/task_0/dashboard/vite.config.js
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
   base: './',
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   server: {
     host: '0.0.0.0',
     port: 5173,
   },
-})
+});
