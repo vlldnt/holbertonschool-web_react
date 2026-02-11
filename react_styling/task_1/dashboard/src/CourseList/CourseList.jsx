17179c2d077362120ef591184de7cf91fd8ec26b
vlldnt <vieilledent.adrien@gmail.com>
2026-02-11 11:08:46 +0000

    wrap tabl in one container

--- react_styling/task_1/dashboard/src/CourseList/CourseList.jsx
+++ react_styling/task_1/dashboard/src/CourseList/CourseList.jsx
@@ -1,41 +1,41 @@
-import CourseListRow from './CourseListRow';
+import CourseListRow from "./CourseListRow";
 
 function CourseList({ courses = [] }) {
-  if (courses.length === 0) {
-    return (
-      <div className="courses-container w-[80%] mx-auto">
-        <table id="noCourse" className="w-full">
+  return (
+    <div className="courses-container w-[80%] mx-auto">
+      <table id="coursesTable" className="w-full">
+        {courses.length === 0 ? (
           <tbody>
             <CourseListRow
               textFirstCell="No course available yet"
               isHeader={true}
             />
           </tbody>
-        </table>
-      </div>
-    );
-  }
-  return (
-    <div className="courses-container flex justify-center w-[80%] mx-auto">
-      <table id="coursesTable" className="w-full">
-        <thead>
-          <CourseListRow textFirstCell="Available courses" isHeader={true} />
-          <CourseListRow
-            textFirstCell="Course name"
-            textSecondCell="Credit"
-            isHeader={true}
-          />
-        </thead>
-        <tbody>
-          {courses.map((course) => (
-            <CourseListRow
-              key={course.id}
-              textFirstCell={course.name}
-              textSecondCell={course.credit}
-              isHeader={false}
-            />
-          ))}
-        </tbody>
+        ) : (
+          <>
+            <thead>
+              <CourseListRow
+                textFirstCell="Available courses"
+                isHeader={true}
+              />
+              <CourseListRow
+                textFirstCell="Course name"
+                textSecondCell="Credit"
+                isHeader={true}
+              />
+            </thead>
+            <tbody>
+              {courses.map((course) => (
+                <CourseListRow
+                  key={course.id}
+                  textFirstCell={course.name}
+                  textSecondCell={course.credit}
+                  isHeader={false}
+                />
+              ))}
+            </tbody>
+          </>
+        )}
       </table>
     </div>
   );
