9f07d15e06e9aaa0eb06d59aff1bc79921743a3a
vlldnt <vieilledent.adrien@gmail.com>
2026-02-11 11:47:19 +0000

    fix: test must pass and they pass why cjecker doesn't passsss

--- react_styling/task_1/dashboard/src/CourseList/CourseList.spec.js
+++ react_styling/task_1/dashboard/src/CourseList/CourseList.spec.js
@@ -1,18 +1,22 @@
-import {render, screen} from '@testing-library/react'
-import CourseList from './CourseList.jsx'
+import { render, screen } from '@testing-library/react';
+import CourseList from './CourseList';
 
-test('should render 5 different rows when receiving array of courses', () => {
-  const courses = [
-    { id: 1, name: 'Alice', credit: 1 },
-    { id: 2, name: 'Bob', credit: 2 },
-    { id: 3, name: 'Carlos', credit: 4 }
-  ]
-  render(<CourseList courses={courses} />)
-  expect(screen.getAllByRole('row').length).toBe(5)
-})
+describe('CourseList tests', () => {
+  test('renders 5 different rows when receiving an array of courses', () => {
+    const courses = [
+      { id: 1, name: 'ES6', credit: 60 },
+      { id: 2, name: 'Webpack', credit: 20 },
+      { id: 3, name: 'React', credit: 40 },
+    ];
+    render(<CourseList courses={courses} />);
+    const rows = screen.getAllByRole('row');
+    expect(rows).toHaveLength(5);
+  });
 
-test('should render 1 row when receiving empty array', () => {
-  const courses = []
-  render(<CourseList courses={courses} />)
-  expect(screen.getAllByRole('row').length).toBe(1)
-})
+  test('renders 1 row when receiving an empty array', () => {
+    render(<CourseList courses={[]} />);
+    const rows = screen.getAllByRole('row');
+    expect(rows).toHaveLength(1);
+    expect(screen.getByText(/No course available yet/i)).toBeInTheDocument();
+  });
+});
