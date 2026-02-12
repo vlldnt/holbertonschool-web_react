1fc59a22381576f5bd28c673f6a5afcf6c670dd7
vlldnt <vieilledent.adrien@gmail.com>
2026-02-12 21:22:36 +0100

    feat: header updated with tailwindcss

--- react_styling/task_3/dashboard/src/Header/Header.jsx
+++ react_styling/task_3/dashboard/src/Header/Header.jsx
@@ -1,11 +1,16 @@
-import holbertonLogo from "../assets/holberton-logo.jpg";
-import "./Header.css";
+import holbertonLogo from '../assets/holberton-logo.jpg';
 
 function Header() {
   return (
-    <header className="App-header">
-      <img src={holbertonLogo} alt="holberton logo" />
-      <h1>School dashboard</h1>
+    <header className="App-header flex items-center">
+      <img
+        className="h-[250px] w-[250px]"
+        src={holbertonLogo}
+        alt="holberton logo"
+      />
+      <h1 className="text-[var(--main-color)] text-5xl font-bold">
+        School dashboard
+      </h1>
     </header>
   );
 }
