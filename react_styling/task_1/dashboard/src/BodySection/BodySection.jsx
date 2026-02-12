cf9d341fa3bf332e104f6770dd9253343e4c084b
vlldnt <vieilledent.adrien@gmail.com>
2026-02-12 13:41:08 +0100

    changin bodysection with a functin

--- react_styling/task_1/dashboard/src/BodySection/BodySection.jsx
+++ react_styling/task_1/dashboard/src/BodySection/BodySection.jsx
@@ -1,8 +1,12 @@
-const BodySection = ({ title, children }) => (
-  <div className="bodySection">
-    <h2>{title}</h2>
-    {children}
-  </div>
-);
+function BodySection({ title, children }) {
+  return (
+    <>
+      <div className="bodySection">
+        <h2>{title}</h2>
+        {children}
+      </div>
+    </>
+  );
+}
 
 export default BodySection;
