62db82b6acf90c2bc748a1db8b380a98e57086d0
vlldnt <vieilledent.adrien@gmail.com>
2026-02-12 12:58:58 +0100

    test: update CourseListRow tests to wrap components in table structure

--- react_styling/task_1/dashboard/src/CourseList/CourseListRow.spec.js
+++ react_styling/task_1/dashboard/src/CourseList/CourseListRow.spec.js
@@ -2,7 +2,7 @@ import CourseListRow from './CourseListRow';
 import { render, screen } from '@testing-library/react';
 
 describe('course list row tests', () => {
-  test('when isHeader is true and textSecondCell is null, renders one th with colspan=2', () => {
+  test('when isHeader is true and textSecondCell is null, renders one th with colspan=1', () => {
     render(<CourseListRow isHeader={true} textSecondCell={null} />);
     const header = screen.getByRole('columnheader');
     expect(header).toBeInTheDocument();
@@ -10,13 +10,25 @@ describe('course list row tests', () => {
   });
 
   test('when isHeader is true and textSecondCell not null, redner twh th', () => {
-    render(<CourseListRow isHeader={true} textSecondCell={'Bonjour'} />);
+    render(
+      <table>
+        <tbody>
+          <CourseListRow isHeader={true} textSecondCell={'Bonjour'} />
+        </tbody>
+      </table>
+    );
     const headers = screen.getAllByRole('columnheader');
     expect(headers).toHaveLength(2);
   });
 
   test('when isHeader false, renders two td within a tr', () => {
-    render(<CourseListRow isHeader={false} />);
+    render(
+      <table>
+        <tbody>
+          <CourseListRow isHeader={false} />
+        </tbody>
+      </table>
+    );
     const row = screen.getByRole('row');
     expect(row).toBeInTheDocument();
     const cells = screen.getAllByRole('cell');
