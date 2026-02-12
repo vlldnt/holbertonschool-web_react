d91ca0d8911da97eb972d43ee34997f6c5645dd1
vlldnt <vieilledent.adrien@gmail.com>
2026-02-12 16:32:33 +0100

    fix: update Notifications component styles for improved layout and consistency

--- react_styling/task_2/dashboard/src/Notifications/Notifications.jsx
+++ react_styling/task_2/dashboard/src/Notifications/Notifications.jsx
@@ -22,9 +22,9 @@ class Notifications extends React.Component {
 
   render() {
     return (
-      <div className="root-notifications flex flex-col items-end p-1.5">
+      <div className="root-notifications w-full absolute flex flex-col items-end p-1.5">
         <div className="notifications-title">
-          <p className="m-0 mb-[0.4rem] text-right">Your notifications</p>
+          <p className="m-0 mb-1.5 text-right">Your notifications</p>
         </div>
         {this.props.displayDrawer && (
           <>
