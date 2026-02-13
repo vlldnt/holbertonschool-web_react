90e43013fa39db04db8d6c3e3827780437bb93e8
vlldnt <vieilledent.adrien@gmail.com>
2026-02-13 15:25:49 +0100

    fix: notifications

--- react_styling/task_2/dashboard/src/Notifications/Notifications.jsx
+++ react_styling/task_2/dashboard/src/Notifications/Notifications.jsx
@@ -5,8 +5,10 @@ import NotificationItem from './NotificationItem.jsx';
 class Notifications extends React.Component {
   static defaultProps = {
     notifications: [],
-    displayDrawer: true,
+    displayDrawer: false,
     markAsRead: () => {},
+    handleDisplayDrawer: () => {},
+    handleHideDrawer: () => {},
   };
 
   markAsRead(id) {
@@ -21,44 +23,69 @@ class Notifications extends React.Component {
   }
 
   render() {
+    const {
+      displayDrawer,
+      notifications,
+      handleDisplayDrawer,
+      handleHideDrawer,
+    } = this.props;
+
     return (
-      <div className="root-notifications w-full absolute flex flex-col items-end p-1.5">
-        <div className="notifications-title">
-          <p className="m-0 mb-1.5 text-right">Your notifications</p>
-        </div>
-        {this.props.displayDrawer && (
-          <>
-            <div className="notification-items w-1/4 border-2 border-dashed border-(--main-color) p-1.5">
-              {this.props.notifications.length === 0 ? (
-                <p>No new notification for now</p>
-              ) : (
-                <>
-                  <p>Here is the list of notifications</p>
-                  <ul className="list-disc pl-4">
-                    {this.props.notifications.map((notif) => (
-                      <NotificationItem
-                        key={notif.id}
-                        id={notif.id}
-                        type={notif.type}
-                        value={notif.value}
-                        html={notif.html}
-                        markAsRead={this.markAsRead}
-                      />
-                    ))}
-                  </ul>
-                </>
-              )}
-            </div>
-            <button
-              className="close-button w-2 h-2 border-0 bg-transparent absolute top-12 right-[1.2rem]"
-              aria-label="Close"
-              onClick={() => console.log('Close button has been clicked')}
+      <>
+        <div className="root-notifications absolute left-0 right-0 flex flex-col items-end z-50 pt-2 pr-4">
+          <div
+            className="notifications-title cursor-pointer"
+            onClick={handleDisplayDrawer}
+          >
+            <p
+              className={`mb-1.5 text-right text-xs tablet:text-base ${notifications.length > 0 && !displayDrawer ? 'animate-bounce' : ''}`}
             >
-              <img className="w-2 h-2" src={closeButton} alt="close-button" />
-            </button>
-          </>
-        )}
-      </div>
+              Your notifications
+            </p>
+          </div>
+
+          {displayDrawer && (
+            <div className="fixed inset-0 z-50 bg-white tablet:relative tablet:inset-auto tablet:bg-transparent tablet:w-1/4 tablet:border-2 tablet:border-dashed tablet:border-(--main-color) tablet:p-1.5">
+              <div className="w-full h-full p-4 overflow-auto border-2 border-dashed border-(--main-color) tablet:border-0 tablet:p-0">
+                {notifications.length === 0 ? (
+                  <p className="mb-4 tablet:mb-0">
+                    No new notification for now
+                  </p>
+                ) : (
+                  <>
+                    <p className="mb-4 tablet:mb-0">
+                      Here is the list of notifications
+                    </p>
+                    <ul className="list-disc p-1 pl-6 tablet:pl-4">
+                      {notifications.map((notif) => (
+                        <NotificationItem
+                          key={notif.id}
+                          id={notif.id}
+                          type={notif.type}
+                          value={notif.value}
+                          html={notif.html}
+                          markAsRead={this.markAsRead}
+                        />
+                      ))}
+                    </ul>
+                  </>
+                )}
+              </div>
+              <button
+                className="close-button fixed top-4 right-4 w-3 h-3 tablet:w-4 tablet:h-4 tablet:top-12 tablet:right-7 border-0 bg-transparent cursor-pointer z-[1000]"
+                aria-label="Close"
+                onClick={handleHideDrawer}
+              >
+                <img
+                  className="w-full h-full"
+                  src={closeButton}
+                  alt="close-button"
+                />
+              </button>
+            </div>
+          )}
+        </div>
+      </>
     );
   }
 }
