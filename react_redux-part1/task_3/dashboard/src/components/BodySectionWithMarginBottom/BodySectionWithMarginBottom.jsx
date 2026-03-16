cdab6bb2f8d5db1fde65ff851b88d5bdb1cb4afe
vlldnt <vieilledent.adrien@gmail.com>
2026-03-16 13:43:22 +0100

    feat: task 3 files copiying

---
+++ react_redux-part1/task_3/dashboard/src/components/BodySectionWithMarginBottom/BodySectionWithMarginBottom.jsx
@@ -0,0 +1,20 @@
+import { StyleSheet, css } from 'aphrodite';
+import BodySection from '../BodySection/BodySection';
+
+const styles = StyleSheet.create({
+  bodySectionWithMargin: {
+    marginBottom: '40px'
+  }
+});
+
+function BodySectionWithMarginBottom({ title, children }) {
+  return (
+    <div className={css(styles.bodySectionWithMargin)}>
+      <BodySection title={title}>
+        {children}
+      </BodySection>
+    </div>
+  )
+}
+
+export default BodySectionWithMarginBottom;
