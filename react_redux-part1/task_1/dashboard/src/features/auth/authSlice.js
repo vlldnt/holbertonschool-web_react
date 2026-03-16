b4a063d68d7b79d68c22902811af685578a4d83f
vlldnt <vieilledent.adrien@gmail.com>
2026-03-16 10:35:16 +0100

    chore: moving tests file to tests forlder

--- react_redux-part1/task_1/dashboard/src/features/auth/authSlice.js
+++ react_redux-part1/task_1/dashboard/src/features/auth/authSlice.js
@@ -1,4 +1,4 @@
-import { createSlice, isAction } from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
   user: {
@@ -12,12 +12,12 @@ const authSlice = createSlice({
   name: 'auth',
   initialState,
   reducers: {
-    login(state, action) {
+    login: (state, action) => {
       state.user.email = action.payload.email;
       state.user.password = action.payload.password;
       state.isLoggedIn = true;
     },
-    logout(state) {
+    logout: (state) => {
       state.user.email = '';
       state.user.password = '';
       state.isLoggedIn = false;
